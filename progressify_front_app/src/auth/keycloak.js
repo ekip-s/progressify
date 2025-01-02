e7a3ce1f67dc753df97d616ce33e50a0404bb6fa
Ekips <b9955047856@gmail.com>
2025-01-02 17:53:30 +0300

    add auth url

--- progressify_front_app/src/auth/keycloak.js
+++ progressify_front_app/src/auth/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "https://darkmatterauth.ru",
+  url: "https://darkmatterauth.ru:8443",
   realm: "dark_matter",
   clientId: "progressify_react_app",
 });
